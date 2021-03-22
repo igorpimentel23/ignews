ec7a68fe92e48a634bdba62b2c3b94b8e0d3b1b1
Igor <igorpimentel@id.uff.br>
2021-03-22 20:25:20 -0300

    Implementing dynamic page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import styled from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styled.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styled.title}>Hello World</h1>
+    </>
   )
 }

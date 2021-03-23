ec15b2e71e8a41b37bf6f332227d6f920b87d719
Igor <igorpimentel@id.uff.br>
2021-03-23 10:01:12 -0300

    Creating Home page

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function subscribeButton() {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
